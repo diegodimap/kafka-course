caf803ed020ba4fbb8d4a82a8f1fea8281010840
Diego Oliveira <diegodimap@gmail.com>
2022-10-02 08:40:05 -0300

    Updated code with instructions to run kafka on windows.

--- kafka-basics/src/main/java/org/example/ProducerDemo.java
+++ kafka-basics/src/main/java/org/example/ProducerDemo.java
@@ -1,2 +1,54 @@
-package org.example;public class ProducerDemo {
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Testing!");
+
+        //create producer configs
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "testing kafka");
+
+        //send data async
+        producer.send(producerRecord);
+
+        //flush and close the producer
+        producer.flush();
+        producer.close();
+
+    }
+
+    /*
+        download kafka: https://kafka.apache.org/downloads
+
+        go to the downloaded folder:
+        zookeeper-server-start.bat ..\..\config\zookeeper.properties
+
+        kafka-server-start.bat ..\..\config\server.properties
+
+        kafka-topics.bat --bootstrap-server 127.0.0.1:9092 --create --topic demo_java --partitions 3 --replication-factor 1
+
+        kafka-console-consumer.bat --bootstrap-server 127.0.0.1:9092 --topic demo_java
+
+     */
+
 }
